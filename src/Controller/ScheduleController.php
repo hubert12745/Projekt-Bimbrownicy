6e4133d29609ef1459456d869a16a7a5d9c70e8f
Milosz Krzywy <krzywymilosz@outlook.com>
2025-01-11 16:30:53 +0100

    html, js and styles

--- src/Controller/ScheduleController.php
+++ src/Controller/ScheduleController.php
@@ -10,83 +10,83 @@ class ScheduleController
 {
     public function indexAction(Templating $templating, Router $router): ?string
     {
-        $schedules = Schedule::findAll();
+//        $schedules = Schedule::findAll();
         $html = $templating->render('schedule/index.html.php', [
-            'schedules' => $schedules,
+          //  'schedules' => $schedules,
             'router' => $router,
         ]);
         return $html;
     }
 
-    public function showAction(int $scheduleId, Templating $templating, Router $router): ?string
-    {
-        $schedule = Schedule::find($scheduleId);
-        if (! $schedule) {
-            throw new NotFoundException("Missing schedule with id $scheduleId");
-        }
-
-        $html = $templating->render('schedule/show.html.php', [
-            'schedule' => $schedule,
-            'router' => $router,
-        ]);
-        return $html;
-    }
-
-    public function createAction(?array $requestPost, Templating $templating, Router $router): ?string
-    {
-        if ($requestPost) {
-            $schedule = Schedule::fromArray($requestPost);
-            // @todo missing validation
-            $schedule->save();
-
-            $path = $router->generatePath('schedule-index');
-            $router->redirect($path);
-            return null;
-        } else {
-            $schedule = new Schedule();
-        }
-
-        $html = $templating->render('schedule/create.html.php', [
-            'schedule' => $schedule,
-            'router' => $router,
-        ]);
-        return $html;
-    }
-
-    public function editAction(int $scheduleId, ?array $requestPost, Templating $templating, Router $router): ?string
-    {
-        $schedule = Schedule::find($scheduleId);
-        if (! $schedule) {
-            throw new NotFoundException("Missing schedule with id $scheduleId");
-        }
-
-        if ($requestPost) {
-            $schedule->fill($requestPost);
-            // @todo missing validation
-            $schedule->save();
-
-            $path = $router->generatePath('schedule-index');
-            $router->redirect($path);
-            return null;
-        }
-
-        $html = $templating->render('schedule/edit.html.php', [
-            'schedule' => $schedule,
-            'router' => $router,
-        ]);
-        return $html;
-    }
-
-    public function deleteAction(int $scheduleId, Router $router): ?string
-    {
-        $schedule = Schedule::find($scheduleId);
-        if (! $schedule) {
-            throw new NotFoundException("Missing schedule with id $scheduleId");
-        }
-
-        $schedule->delete();
-        $path = $router->generatePath('schedule-index');
-        $router->redirect($path);
-        return null;
-    }
+//    public function showAction(int $scheduleId, Templating $templating, Router $router): ?string
+//    {
+//        $schedule = Schedule::find($scheduleId);
+//        if (! $schedule) {
+//            throw new NotFoundException("Missing schedule with id $scheduleId");
+//        }
+//
+//        $html = $templating->render('schedule/show.html.php', [
+//            'schedule' => $schedule,
+//            'router' => $router,
+//        ]);
+//        return $html;
+//    }
+//
+//    public function createAction(?array $requestPost, Templating $templating, Router $router): ?string
+//    {
+//        if ($requestPost) {
+//            $schedule = Schedule::fromArray($requestPost);
+//            // @todo missing validation
+//            $schedule->save();
+//
+//            $path = $router->generatePath('schedule-index');
+//            $router->redirect($path);
+//            return null;
+//        } else {
+//            $schedule = new Schedule();
+//        }
+//
+//        $html = $templating->render('schedule/create.html.php', [
+//            'schedule' => $schedule,
+//            'router' => $router,
+//        ]);
+//        return $html;
+//    }
+//
+//    public function editAction(int $scheduleId, ?array $requestPost, Templating $templating, Router $router): ?string
+//    {
+//        $schedule = Schedule::find($scheduleId);
+//        if (! $schedule) {
+//            throw new NotFoundException("Missing schedule with id $scheduleId");
+//        }
+//
+//        if ($requestPost) {
+//            $schedule->fill($requestPost);
+//            // @todo missing validation
+//            $schedule->save();
+//
+//            $path = $router->generatePath('schedule-index');
+//            $router->redirect($path);
+//            return null;
+//        }
+//
+//        $html = $templating->render('schedule/edit.html.php', [
+//            'schedule' => $schedule,
+//            'router' => $router,
+//        ]);
+//        return $html;
+//    }
+//
+//    public function deleteAction(int $scheduleId, Router $router): ?string
+//    {
+//        $schedule = Schedule::find($scheduleId);
+//        if (! $schedule) {
+//            throw new NotFoundException("Missing schedule with id $scheduleId");
+//        }
+//
+//        $schedule->delete();
+//        $path = $router->generatePath('schedule-index');
+//        $router->redirect($path);
+//        return null;
+//    }
 }
